1527b9e21e232bedde3a8427eafa168cea015522
Vicky_Kumar <41174782+vickykumar123@users.noreply.github.com>
2026-04-21 20:42:12 +0530

    test: seed src/utils/logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,19 +1,6 @@
 import logging
-import sys
 
-
-def setup_logger(name):
-    """Create and configure a logger with a StreamHandler to stdout.
-
-    The logger is set to INFO level and uses the format:
-    '[%(asctime)s] %(name)s %(levelname)s: %(message)s'.
-    """
+def setup_logger(name: str):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    # Add handler only if the logger doesn't have one already
-    if not logger.handlers:
-        handler = logging.StreamHandler(sys.stdout)
-        formatter = logging.Formatter('[%(asctime)s] %(name)s %(levelname)s: %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
     return logger
