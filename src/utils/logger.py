ad3a03910dfe7069605b9d6ec8abb467bb2c96b1
Vicky_Kumar <41174782+vickykumar123@users.noreply.github.com>
2026-05-02 15:36:36 +0530

    test: seed src/utils/logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,23 +1,6 @@
 import logging
 
-def setup_logger(name):
-    """
-    Create or retrieve a logger with the given name.
-    Configures the logger to log INFO level messages to the console with a
-    specific format. Duplicate handlers are avoided.
-    """
+def setup_logger(name: str):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-
-    # Avoid adding duplicate console handlers
-    for handler in logger.handlers:
-        if isinstance(handler, logging.StreamHandler):
-            return logger
-
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-    formatter = logging.Formatter('[%(asctime)s] %(name)s %(levelname)s: %(message)s')
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-
+    logger.setLevel(logging.DEBUG)
     return logger
